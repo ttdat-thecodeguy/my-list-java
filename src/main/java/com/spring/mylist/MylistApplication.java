064a6cd213a23eadd53ca244de76f0e70675c2e9
cutecat-vn <55250111+ttdat-thecodeguy@users.noreply.github.com>
2024-07-26 22:17:45 +0700

    [DatTruongThanh][Tasks]Init-MyList-Project

---
+++ src/main/java/com/spring/mylist/MylistApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylistApplication.class, args);
+	}
+
+}
